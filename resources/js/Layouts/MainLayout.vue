3a8a65536d3dba27608d661ae6b309e8b19c2ae4
Gianneves <gianneves@hotmail.com>
2024-06-07 13:40:11 -0300

    feat: Extracting Tailwind Styles Using @apply Directive

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -10,7 +10,7 @@
                 </div>
                 <div>
                     <Link :href="route('listing.create')"
-                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing
+                        class="btn-primary">+ New Listing
                     </Link>
                 </div>
             </nav>
