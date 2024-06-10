cc084749f2a88b0cfded328adc5f9ba641fe9c88
Gianneves <gianneves@hotmail.com>
2024-06-10 11:02:28 -0300

    feat: Composables - Reusable Logic in Vue

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -45,7 +45,9 @@ import PriceTag from "@/Components/PriceTag.vue";
 import Box from "@/Components/UI/Box.vue";
 import MainLayout from "@/Layouts/MainLayout.vue";
 
-import { ref, computed } from "vue";
+import { ref } from "vue";
+
+import { useMonthlyPayment } from "@/Composables/useMonthlyPayment";
 
 const interestRate = ref(2.5);
 const duration = ref(25);
@@ -54,11 +56,5 @@ const props = defineProps({
   listing: Object,
 });
 
-const monthlyPayment = computed(() => {
-  const principle = props.listing.price;
-  const monthlyInterest = interestRate.value / 100 / 12;
-  const numberOfPaymentMonths = duration.value * 12;
-
-  return principle * monthlyInterest * (Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
-});
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration);
 </script>
