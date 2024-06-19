4f07b35631fdc27b98d6f7dba3fa007180b4786b
Gianneves <gianneves@hotmail.com>
2024-06-19 16:36:18 -0300

    feat: Signing-In Logic - Authentication Process Explained

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class AuthController extends Controller
 {
     public function create() {
-        
+        return Inertia::render('AuthPage/Login');
     }
 
-    public function store() {
+    public function store(Request $request) {
 
+        if (!Auth::attempt( $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]), true)) {
+            throw ValidationException::withMessages([
+                'email' =>  'Authentication failed'
+            ]);
+        }
+
+        $request->session()->regenerate();
+        
+        return redirect()->intended();
     }
 
     public function destroy() {
---
+++ resources/js/Pages/AuthPage/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <MainLayout>
+        <form @submit.prevent="login">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="email" class="label">E-mail (username)</label>
+                <input type="text" id="email" class="input" v-model="form.email">
+                <div class="input-error">Potential errors</div>
+            </div>
+            <div class="mt-4">
+                <label for="password" class="label">Password</label>
+                <input type="password" id="password" class="input" v-model="form.password">
+                <div class="input-error">Potential errors</div>
+            </div>
+            <div class="mt-4">
+                <button class="btn-primary w-full" type="submit">Login</button>
+            </div>
+        </div>
+    </form>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    email: null,
+    password: null
+});
+
+const login = () => form.post(route('login.store'));
+</script>
