0449c05b74292baef5b7c80cb7a39d7d648a84b6
Gianneves <gianneves@hotmail.com>
2024-06-20 08:11:50 -0300

    feat: Authentication Process

--- resources/js/Pages/AuthPage/Login.vue
+++ resources/js/Pages/AuthPage/Login.vue
@@ -3,14 +3,14 @@
         <form @submit.prevent="login">
         <div class="w-1/2 mx-auto">
             <div>
-                <label for="email" class="label">E-mail (username)</label>
+                <label for="email" class="label">E-mail</label>
                 <input type="text" id="email" class="input" v-model="form.email">
-                <div class="input-error">Potential errors</div>
+                <div class="input-error" v-if="form.errors.email">{{ form.errors.email }}</div>
             </div>
             <div class="mt-4">
                 <label for="password" class="label">Password</label>
                 <input type="password" id="password" class="input" v-model="form.password">
-                <div class="input-error">Potential errors</div>
+                <div class="input-error" v-if="form.errors.password">{{ form.errors.password }}</div>
             </div>
             <div class="mt-4">
                 <button class="btn-primary w-full" type="submit">Login</button>
