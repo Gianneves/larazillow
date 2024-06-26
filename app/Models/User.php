4d7a3bf74f5947ed801bdb91375dd5367cc3b212
Gianneves <gianneves@hotmail.com>
2024-06-26 14:39:43 -0300

    feat: Creating a One-to-Many Database Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
@@ -55,5 +56,9 @@ class User extends Authenticatable
             get: fn ($value) => $value,
             set: fn ($value) => Hash::make($value),
         );
+    }
+
+    public function listings(): HasMany {
+        return $this->hasMany(Listing::class, 'by_user_id');
     }
 }
