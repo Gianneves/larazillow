58cd91295bcbbbf254894b83bdd62427ecd76026
Gianneves <gianneves@hotmail.com>
2024-07-11 12:55:58 -0300

    feat:  Uploading Multiple Images (In Vue)

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,18 +1,43 @@
 <template>
-
-    <form 
-        enctype="multipart/form-data"
-        method="POST"
-        :action="route('realtor.listing.image.store', { listing: listing.id })"
-        >
-        <input type="file" multiple name="images[]">
-        <button type="submit">Send</button>
-    </form>
-
+    <MainLayout>
+        <Box>
+            <template #header>Upload New Images</template>
+            <form @submit.prevent="upload">
+                <input type="file" multiple @input="addFiles">
+                <button type="submit" class="btn-outline">Upload</button>
+                <button type="reset" @click="reset" class="btn-outline">Reset</button>
+            </form>
+        </Box>
+    </MainLayout>
 </template>
 
 <script setup>
-defineProps({
+import Box from '@/Components/UI/Box.vue';
+import MainLayout from '@/Layouts/MainLayout.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const props = defineProps({
     listing: Object
 });
+
+const form = useForm({
+    images: []
+});
+
+const upload = () => {
+    form.post(
+        route('realtor.listing.image.store', { listing: props.listing.id }),
+        {
+            onSuccess: () => form.reset('images')
+        }
+    );
+}
+
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image)
+    }
+}
+
+const reset = () => form.reset('images');
 </script>
